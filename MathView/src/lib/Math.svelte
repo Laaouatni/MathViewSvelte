4af469bbddd8a96350ae7ad03806abb4cbbe86ab
laaouatni <laaouatni.anas@outlook.it>
2023-01-24 21:28:50 +0100

    finished

--- MathView/src/lib/Math.svelte
+++ MathView/src/lib/Math.svelte
@@ -1,11 +1,15 @@
 <script>
   export let value;
+
   import MathColumn from "./MathColumn.svelte";
+  import mathToJson from "../scripts/mathToJson";
   import MathRow from "./MathRow.svelte";
+  
+  $: data = mathToJson(value);
 </script>
 
 {#if value.operation === "/"}
-  <MathColumn data={value} />
+  <MathColumn {data} />
   {:else}
-  <MathRow data={value} />
+  <MathRow {data} />
 {/if}
--- MathView/src/routes/+page.svelte
+++ MathView/src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script>
-  import mathToJson from "../scripts/mathToJson";
   import Math from "../lib/Math.svelte";
-
-  let result = mathToJson("1/2");
-  console.log(result);
 </script>
 
 <div class="overflow-auto p-4">
-  <Math value={result} />
+  <Math value="1+2" />
 </div>
