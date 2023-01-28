910bdca68797455687380180d1eae2838769646d
laaouatni <laaouatni.anas@outlook.it>
2023-01-28 16:49:25 +0100

    mini charge in hover style, add linked the github repo in package json

--- MathView/src/lib/Number.svelte
+++ MathView/src/lib/Number.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="grid place-items-center border p-4 rounded-md hover:scale-110 hover:bg-gray-50 hover:shadow-inner bg-white transition"
+  class="grid place-items-center border p-4 rounded-md hover:bg-gray-50 hover:shadow-inner bg-white transition"
 >
   {value}
 </div>
